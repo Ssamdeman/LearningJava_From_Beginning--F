e9c50ff05d7dc8cd9f534a6b6452ff2c7ec492d8
ssam135 <sultan.samid@gmail.com>
2022-02-07 09:07:45 -0500

    worked on hw2 for cs310

--- IntelliJEnvironment/Spring2022UmassBoston/cs310Projedcts/pa0_skeleton/LineUsage.java
+++ IntelliJEnvironment/Spring2022UmassBoston/cs310Projedcts/pa0_skeleton/LineUsage.java
@@ -1,19 +1,52 @@
 package pa0;
 // Import packages as needed
 
+import java.util.HashMap;
+
 // LineUsageData.java: Handle one line's data, using a Map
+
+//---------------------------------------------------------------//
+// The problem is that I dont know how will I get the data in the LineUsage.
+// I know LineUsage crates HashTable and save it. questions would be the user data
+// is coming? And how the Friequesn counter got it here.
+
+
+// I think my problem is that I am approaching this probelm as if this is the main function
+// I think there are other ways apporeach it.
+
+//To list: how to fill the hashtable: -->
+/*HashMap<String, Integer> st = new HashMap<String, Integer>();
+        while (!StdIn.isEmpty()) {
+        String word = StdIn.readString();
+        if (word.length() < minlen) continue;
+        if (!st.containsKey(word)) st.put(word, 1);
+        else {
+        st.put(word, st.get(word) + 1);
+        }
+    }*/
 public class LineUsage {
-	// Variables here
+    // Variables here
+    
+    HashMap<String, Integer> user_count = new HashMap<String, Integer>();
 
-	// Constructor
-	public LineUsage() {
-	}
+    // Constructor
+    public LineUsage() {
+        this.user_count = new HashMap<String, Integer>();
+    }
 
-	// add one sighting of a user on this line
-	public void addObservation(String username) {
-	}
+    // add one sighting of a user on this line
+    public void addObservation(String username) {
+        this.user_count[username] += 1;
 
-	// find the user with the most sightings on this line
-	public Usage findMaxUsage() {
-	}
+    }
+
+    // find the user with the most sightings on this line
+    public pa0.Usage findMaxUsage() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
--- IntelliJEnvironment/Spring2022UmassBoston/cs310Projedcts/pa0_skeleton/Usage.java
+++ IntelliJEnvironment/Spring2022UmassBoston/cs310Projedcts/pa0_skeleton/Usage.java
@@ -2,6 +2,12 @@ package pa0;
 
 // One user's record on one line: how many times
 // this user has been seen on this line
+//---------------------------------------------------------------//
+//I dont I understand of I did this thing right or not.
+//becasue it is asking this: Ask help from TA.
+///"a, create a Usage object, add one to its count, and
+//then print out its username and count using the getters."
+//---------------------------------------------------------------//
 public class Usage {
     private int count;
     private String userName;
@@ -26,7 +32,7 @@ public class Usage {
 
     /*public static void main(String[] args) {
         Usage objc = new Usage("Hello", 10);
+        System.out.print(objc.getUser() + " ");
         System.out.println(objc.getCount());
-
     }*/
 }
