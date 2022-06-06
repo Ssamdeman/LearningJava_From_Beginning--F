816526b804a23167343739aff82b8827f13c1113
ssam135 <sultan.samid@gmail.com>
2022-06-06 11:54:19 -0400

    course online for Algorith from PC

--- IntelliJEnvironment/Algorithms, Part I or II (Coursera/Week1/Percolation.java
+++ IntelliJEnvironment/Algorithms, Part I or II (Coursera/Week1/Percolation.java
@@ -6,9 +6,17 @@
 
 package Week1;
 
+///TODO Watch this: https://www.youtube.com/watch?v=Gz-K8HLkzeM
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     private boolean[][] matrix;
+    private int size_Matrix;
+
+    WeightedQuickUnionUF full;
+    WeightedQuickUnionUF perc;
 
 
     public Percolation(int N) {
@@ -16,13 +24,57 @@ public class Percolation {
         if (N < 0) throw new IllegalArgumentException("");
 
         matrix = new boolean[N][N];
+        size_Matrix = N;
+        full = new WeightedQuickUnionUF(N * N);
+        perc = new WeightedQuickUnionUF((N * N) + 2);
+
 
     }
 
     //open thing given the 1-p
     public void open(int row, int col) {
+        //corner cases
+        if (row < 0 || row > size_Matrix) {
+            throw new IllegalArgumentException("");
+        }
+        if (col < 0 || col > size_Matrix) {
+            throw new IllegalArgumentException("");
+        }
+
+        //opening sites with randomly.
+        //  matrix[row][col] = true;//Testing purposes
+        for (int i = 0; i < size_Matrix; i++) {
+            for (int j = 0; i < size_Matrix; i++) {
+                if (StdRandom.bernoulli()) matrix[i][j] = true;
+
+            }
+
+        }
 
     }
+
+    ///converting perc into the 2d system
+    private int encode(int row, int col) {
+
+
+    }
+
+    //1 2 3
+    //0,0, 0,1 0,2
+
+    public boolean isOpen(int row, int col) {
+        //corner cases
+        if (row < 0 || row > size_Matrix) {
+            throw new IllegalArgumentException("");
+        }
+        if (col < 0 || col > size_Matrix) {
+            throw new IllegalArgumentException("");
+        }
+        return matrix[row][col];
+
+
+    }
+
 
     //testing things
     public void print() {
@@ -40,6 +92,7 @@ public class Percolation {
     public static void main(String[] args) {
 
         Percolation persolation = new Percolation(3);
+        persolation.open(1, 1);
         persolation.print();
 
 
