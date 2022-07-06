25acc9c495edc6db53ce548dc7beeaf3d392a686
ssam135 <sultan.samid@gmail.com>
2022-07-06 13:10:34 -0400

    from pc

---
+++ IntelliJEnvironment/Algorithms, Part I or II (Coursera/Week2/queues/Deque.java
@@ -0,0 +1,44 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+package Week2.queues;
+
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+
+    // construct an empty deque
+    public Deque()
+
+    // is the deque empty?
+    public boolean isEmpty()
+
+    // return the number of items on the deque
+    public int size()
+
+    // add the item to the front
+    public void addFirst(Item item)
+
+    // add the item to the back
+    public void addLast(Item item)
+
+    // remove and return the item from the front
+    public Item removeFirst()
+
+    // remove and return the item from the back
+    public Item removeLast()
+
+    // return an iterator over items in order from front to back
+    public Iterator<Item> iterator()
+
+    // unit testing (required)
+    public static void main(String[] args)
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ IntelliJEnvironment/Algorithms, Part I or II (Coursera/Week2/queues/Permutation.java
@@ -0,0 +1,13 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+package Week2.queues;
+
+public class Permutation {
+    public static void main(String[] args) {
+
+    }
+}
